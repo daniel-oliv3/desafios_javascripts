7fd1259334ed4468a0df41b37255f0e03ed3fbe9
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-27 16:48:14 -0300

    Upd script

--- script-01.js
+++ script-01.js
@@ -1,6 +1,14 @@
 let N = parseInt(gets());
 
+let pares = [];
+let impares = [];
 
+
+for(let i = 0; i < N; i++){
+    let num = parseInt(gets());
+
+    num % 2 === 0 ? pares.push(num) : impares.push(num);
+}
 
 
 
