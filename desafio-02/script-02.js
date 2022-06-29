32aeecc2fe2a26c0c31025571afb9a5ca4ce8312
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-29 15:55:23 -0300

    Update script-02.js

---
+++ desafio-02/script-02.js
@@ -0,0 +1,20 @@
+let entrada = "banana abacaxi melão uva mamão tangerina";
+
+let arr = entrada.split(' ');
+
+console.log(arr.sort((a, b) => {
+    if(a > b) return 1;
+    if(b > a) return -1;
+    if(a === b) return 0;
+}).sort((a, b) => b.length - a.length)
+);
+
+
+
+
+
+
+
+
+
+
