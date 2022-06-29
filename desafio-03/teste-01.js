349e74ea19308b47b9703a40d53180daab22a242
Daniel Oliveira <39778932+daniel-oliv3@users.noreply.github.com>
2022-06-29 17:06:20 -0300

    update

--- desafio-03/teste-01.js
+++ desafio-03/teste-01.js
@@ -2,8 +2,6 @@ let entrada = [7, 8, 10, 8, 260, 4, 10, 10];
 
 entrada.sort((a, b) => a - b);
 
-//console.log(entrada);
-
-for(indice in entrada){
-    console.log(indice);
+for(elemento of entrada){
+    console.log(elemento);
 }
